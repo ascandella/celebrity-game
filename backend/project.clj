bd2c19155a5c530929721b2ee1d6ab5b9fc66e9c
Aiden Scandella <git@sca.ndella.com>
2020-04-05 15:03:14 -0700

    Add aleph and websocket handlers

--- backend/project.clj
+++ backend/project.clj
@@ -3,15 +3,26 @@
   :url "https://github.com/ascandella/celebrity-game"
   :license {:name "MIT"
             :url "https://opensource.org/licenses/MIT"}
+
   :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/core.async "0.4.474"]
                  [ring/ring-core "1.6.3"]
-                 [ring/ring-jetty-adapter "1.6.3"]]
+                 [aleph "0.4.6"]
+                 [compojure "1.6.1"]]
+
   :main ^:skip-aot celebrity.core
+
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}
-             :test {:dependencies [[ring/ring-mock "0.4.0"]]} }
+
+  :profiles
+  {:uberjar {:aot :all}
+   :test {:dependencies [[ring/ring-mock "0.4.0"]]}}
 
   :ring
   {:handler celebrity.core/handler
+   ;; keep ring from trying to import test things
+   :reload-paths ["src"]
+   ;; for running in docker
    :open-browser? false}
+  ;; provides "lein ring server"
   :plugins [[lein-ring "0.12.5"]])
