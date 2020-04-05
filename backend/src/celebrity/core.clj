b86eb450d3762ae99c5bd25de98ebcc5d95e02f2
Aiden Scandella <git@sca.ndella.com>
2020-04-05 09:19:10 -0700

    Add main jetty handler to backend, production builds

--- backend/src/celebrity/core.clj
+++ backend/src/celebrity/core.clj
@@ -1,13 +1,13 @@
 (ns celebrity.core
+  (:require [ring.adapter.jetty :refer [run-jetty]])
   (:gen-class))
-
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
 
 
 (defn handler [request]
   {:status 200
    :headers {"Content-Type" "text/html"}
    :body "Hello, World"})
+
+(defn -main
+  [& args]
+  (run-jetty handler {:port (Integer/valueOf (or (System/getenv "PORT") "3000"))}))
