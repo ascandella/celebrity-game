aee067d5d4b49c403990af981021bdc8423de6ca
Aiden Scandella <git@sca.ndella.com>
2020-04-05 15:06:33 -0700

    Add aleph and websocket handlers (#12)

--- backend/src/celebrity/core.clj
+++ backend/src/celebrity/core.clj
@@ -1,13 +1,58 @@
 (ns celebrity.core
-  (:require [ring.adapter.jetty :refer [run-jetty]])
+  (:require [compojure.core :as compojure :refer [GET]]
+            [ring.middleware.params :as params]
+            [compojure.route :as route]
+            [aleph.http :as http]
+            [byte-streams :as bs]
+            [manifold.stream :as s]
+            [manifold.deferred :as d]
+            [manifold.bus :as bus]
+            [clojure.core.async :as a])
   (:gen-class))
 
+(def non-websocket-request
+  {:status 400
+   :headers {"Content-type" "text/plain"}
+   :body "Expected a websocket request."})
 
-(defn handler [request]
+;; TODO(aiden) remove this
+(defn echo-handler
+  [req]
+  (-> (d/let-flow [socket (http/websocket-connection req)]
+         ;; connect the socket to itself
+         (s/connect socket socket))
+      (d/catch
+          (fn [_]
+            non-websocket-request))))
+
+(defn game-handler
+  [req]
+  (->
+   (d/let-flow [conn (http/websocket-connection req)]
+        ;; TODO this
+               nil)
+   (d/catch
+       (fn [_]
+         non-websocket-request))))
+
+(defn health-handler
+  [req]
   {:status 200
-   :headers {"Content-Type" "text/html"}
-   :body "Hello, World"})
+   :headers {"Content-type" "text/plain"}
+   :body "OK"})
+
+(def handler
+  (params/wrap-params
+   (compojure/routes
+    (GET "/game" [] game-handler)
+    (GET "/echo" [] echo-handler)
+    (GET "/health" [] health-handler)
+    (route/not-found "No such page."))))
+
+(defn get-port
+  ([] (get-port "PORT"))
+  ([key] (Integer/valueOf (or (System/getenv key) "3030"))))
 
 (defn -main
   [& args]
-  (run-jetty handler {:port (Integer/valueOf (or (System/getenv "PORT") "3000"))}))
+  (http/start-server handler {:port (get-port)}))
--- backend/test/celebrity/core_test.clj
+++ backend/test/celebrity/core_test.clj
@@ -3,7 +3,18 @@
             [celebrity.core :refer :all]
             [ring.mock.request :as mock]))
 
-(deftest a-test
-  (testing "Home page is 200"
-    (is (= (:status (handler (mock/request :get "/")))
-           200))))
+(deftest health-test
+  (testing "Health page is 200"
+    (let [response (handler (mock/request :get "/health"))]
+      (is (= (:body response) "OK"))
+      (is (= (:status response) 200)))))
+
+(deftest not-found
+  (testing "Any random page is 404"
+    (is (= (:status (handler (mock/request :get "/non-existent")))
+           404))))
+
+(deftest non-websocket-game
+  (testing "Non-websocket connection to game fails"
+    (let [response (handler (mock/request :get "/game"))]
+      (is (= (:status response) 400)))))
