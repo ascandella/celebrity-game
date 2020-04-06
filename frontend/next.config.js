a0dc369e9f578d7512523e2dbe5ee62797e8e0b8
Aiden Scandella <git@sca.ndella.com>
2020-04-05 21:02:20 -0700

    Re-add default config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,6 +3,7 @@ const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
 module.exports = (phase, { defaultConfig }) => {
   if (phase === PHASE_DEVELOPMENT_SERVER) {
     return {
+      ...defaultConfig,
       env: {
         // default clojure backend in development / docker
         apiBase: "ws://localhost:3030",
@@ -11,6 +12,7 @@ module.exports = (phase, { defaultConfig }) => {
   }
 
   return {
+    ...defaultConfig,
     env: {
       apiBase: process.env.CELEBRITY_BACKEND,
     },
