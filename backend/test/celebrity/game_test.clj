f90341c09f8258b947ee3b2f645df7d9d1073ee3
Aiden Scandella <git@sca.ndella.com>
2020-04-10 10:02:02 -0700

    Remove more global state; into handler loop (#40)
    * Remove more global state; into handler loop
    
    * Fix joining

--- backend/test/celebrity/game_test.clj
+++ backend/test/celebrity/game_test.clj
@@ -2,6 +2,7 @@
   (:require [celebrity.game :refer :all]
             [celebrity.protocol :as proto]
             [clojure.test :refer :all]
+            [clojure.core.async :as a]
             [manifold.deferred :as d]
             [manifold.stream :as s]))
 
@@ -28,25 +29,25 @@
 (deftest create-game-connects-client
   (testing "we can create a new game"
     (let [client   (s/stream)
-          params   {:create {:name "aiden"}
+          params   {:name   "aiden"
                     :config {:foo "bar"}}
           registry (atom {})
-          val        (create-game params client {:registry registry})]
+          val      (create-game params client {:registry registry})]
       (is (= val :pending))
       ;; this should succeed because the server is listening
-      @(d/let-flow
-        [create-response (s/try-take! client 500)]
-        (let [json-response (proto/parse-message create-response)
-              code (:room-code json-response)]
-          (is (not (nil? code)))
-          (is (= (:name json-response) "aiden"))
-          (is (:joinable? (get @registry code)))
-          (is (= (get-in @registry [code :config :foo]) "bar"))
-          (is @(s/try-put! client "{\"ping\" true}" 500))
-          (d/let-flow [ping-response (s/try-take! client 500)]
-                      (let [result-parsed (proto/parse-message ping-response)]
-                        (is (:pong result-parsed))
-                        (is (> (count (:client-id result-parsed)) 20)))))))))
+      (let [create-response @(s/try-take! client 500)
+            json-response   (proto/parse-message create-response)
+            code            (:room-code json-response)]
+        (is (not (nil? code)))
+        (is (= (:name json-response) "aiden"))
+        (is (:joinable? (get @registry code)))
+        (is (= (get-in @registry [code :config :foo]) "bar"))
+        (is @(s/try-put! client "{\"ping\" true}" 500))
+
+        (let [ping-response @(s/try-take! client 500)
+              result-parsed (proto/parse-message ping-response)]
+          (is (:pong result-parsed))
+          (is (> (count (:client-id result-parsed)) 20)))))))
 
 (deftest join-game-does-not-exist
   (testing "try to join a game that doesn't exist"
@@ -66,19 +67,44 @@
           response (join client join-data {:registry registry})]
       (is (=
            (:error response)
-           "Room is full")))))
-
-(deftest client-disconnect-with-active-players
-  (testing "We decrement player count on disconnect"
-    (let [code "test"
-          registry (atom {code {:player-count 2}})
-          new-registry (on-client-disconnect registry code)]
-      (is (= 1
-             (get-in new-registry [code :player-count]))))))
+           "Room is full"))
+      (s/close! client))))
 
 (deftest client-last-player
   (testing "We cull old games when the last client disconnects"
-    (let [code "test"
-          registry (atom {code {:player-count 1}})
-          new-registry (on-client-disconnect registry code)]
-      (is (nil? (get code new-registry))))))
+    (let [client   (s/stream)
+          params   {}
+          registry (atom {})
+          created  (create-game params client {:registry registry})
+          response @(s/try-take! client 500)
+          parsed   (proto/parse-message response)]
+      (is (= :pending created))
+      (s/close! client)
+      (Thread/sleep 50)
+      ;; either the game is deregistered, or the server conn should be closed
+      (when-let [game-state (get @registry (:room-code parsed))]
+        (let [bus (:bus game-state)]
+          ;; NOTE: this will block
+          (is (false? (a/>!! bus "test message"))))))))
+
+(deftest created-game-can-be-joined
+  (testing "After creating a game, another client can join"
+    (let [creator  (s/stream)
+          joiner   (s/stream)
+          params   {:name "creator"}
+          registry (atom {})
+          _        (create-game params creator {:registry registry})
+          response @(s/try-take! creator 500)
+          parsed   (proto/parse-message response)
+          code     (:room-code parsed)]
+      (is (= :pending
+             (join joiner {:room-code code
+                           :name "joiner"}
+                   {:registry registry})))
+      (let [join-response @(s/try-take! joiner 500)
+            parsed-join   (proto/parse-message join-response)]
+        (is (:success parsed-join))
+        (is (= "joiner" (:name parsed-join)))
+        (is (= code (:room-code parsed-join))))
+      (s/close! creator)
+      (s/close! joiner))))
