09bfd9f35658060a18a088bd7a361aaf0ec37f10
Aiden Scandella <git@sca.ndella.com>
2020-04-10 18:18:31 -0700

    Convert index to functioncomponent

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,33 +1,20 @@
-import React, { Component } from "react";
+import React, { FunctionComponent } from "react";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
 import GameContainer from "../components/game-container";
 import CelebrityClient from "../clients/celebrity";
 import gameApp from "../reducers/index";
 
-type IndexState = {
-  client: CelebrityClient;
-};
-
 const store = createStore(gameApp);
 
-class Index extends Component<{}, IndexState> {
-  constructor(props: {}) {
-    super(props);
-    const client = new CelebrityClient(store);
+const Index: FunctionComponent<{}> = () => {
+  const client = new CelebrityClient(store);
 
-    this.state = {
-      client,
-    };
-  }
-
-  render(): React.ReactNode {
-    return (
-      <Provider store={store}>
-        <GameContainer client={this.state.client} />
-      </Provider>
-    );
-  }
-}
+  return (
+    <Provider store={store}>
+      <GameContainer client={client} />
+    </Provider>
+  );
+};
 
 export default Index;
