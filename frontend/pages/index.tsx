a5e55f51a5adfb4388ab076e4198aa6379d2fc52
Aiden Scandella <git@sca.ndella.com>
2020-05-02 17:59:16 -0700

    Add hooks for redux devtools

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,11 +1,19 @@
 import React, { FunctionComponent } from "react";
 import { Provider } from "react-redux";
 import thunkMiddleware from "redux-thunk";
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import GameContainer from "../components/game-container";
 import gameApp from "../reducers";
 
-const store = createStore(gameApp, applyMiddleware(thunkMiddleware));
+/* eslint-disable no-underscore-dangle */
+const composeEnhancers =
+  (typeof window !== "undefined" &&
+    (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+const store = createStore(
+  gameApp,
+  composeEnhancers(applyMiddleware(thunkMiddleware))
+);
 
 const Index: FunctionComponent<{}> = () => {
   return (
